669aef943c29f4c29fdab4ac585735ee98639bca
Shivkant Dohaliya <shivkant.dohaliya@exprealty.net>
2025-05-07 17:43:55 +0530

    Add jquery, bootstrap, fontawesome and integrate template

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
-// Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+// Import jQuery, Popper.js, and Bootstrap 4.3.1
+import $ from "jquery"
+// Make jQuery globally available (important for Bootstrap 4 plugins)
+window.$ = $
+window.jQuery = $
+
+import "popper.js"
+import "bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "font-awesome/css/font-awesome.min.css"
+import "./stylesheets/application.css"
