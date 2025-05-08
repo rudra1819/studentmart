d5cb591c26356cee0af04da9c1ddbc6e5efa9198
bharat goswami <nil>
2025-05-08 23:27:43 +0530

    apply devise

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UserController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+end
